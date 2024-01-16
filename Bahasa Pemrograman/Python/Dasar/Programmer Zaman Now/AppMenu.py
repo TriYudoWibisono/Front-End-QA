1cbca30569f567aaad98f33abf2be22acc049afd
Tri Yudo Wibisono <triyudowibisono98@gmail.com>
2024-01-16 16:35:08 +0700

    materi project terakhir python

--- Bahasa Pemrograman/Python/Dasar/Programmer Zaman Now/AppMenu.py
+++ Bahasa Pemrograman/Python/Dasar/Programmer Zaman Now/AppMenu.py
@@ -1,22 +1,51 @@
-import  Function
+# import ini agar kita bisa akses function yang ada di file Function
+import Function
 
-print("Selamat Datang di Contact Apk")
-print("1. Daftar Kontak")
-print("2. Tambah kontak")
-print("3. Hapus kontak")
-print("4. Cari kontak")
+#membuat program untuk manajemen kontak. berikut untuk daftar menunya :
+#Menu
+# 1. Daftar Kontak
+# 2. Tambah kontak
+# 3. Hapus kontak
+# 4. Cari kontak
 
-pilih_menu = int(input("Menu apa yang ingin dilakukan ? : "))
+# Data Kontak :
+# 1. Nama
+# 2. Email
+# 3. No Telepon
 
-if pilih_menu == 1:
-    tambah = Function.tambah()
-    print(tambah)
+#Disini kita membuat 2 file. Appmenu.py untuk membuat programnya dan function.py untuk function2 menu2 yang kita buat.
 
-elif pilih_menu == 2:
-    print("daftar")
-elif pilih_menu == 3 :
-    print("daftar") 
-elif pilih_menu == 4:
-    print("daftar")
-else:
-    print("Menu Tidak Tersedia !")
+# pertama kita buat variabel yang digunakan untuk menampung semua data kontak kita (variabel list dictionary)
+daftar_contak = []
+daftar_contak.append({
+    "nama"    : "Yudo",
+    "email"   : "yuuudoooo@gmail.com",
+    "telepon" : "34533434343242"
+})
+
+# Menu program
+while True:
+    print("Menu")
+    print("1. Daftar Kontak")
+    print("2. Tambah kontak")
+    print("3. Hapus kontak")
+    print("4. Cari kontak")
+    print("0. Keluar Program")
+
+    menu = input("Pilih menu : ")
+
+    if menu == "0":
+        break
+    elif menu == "1":
+        daftar = Function.display(daftar_contak)
+    elif menu == "2":
+        tambah = Function.tambah()
+        daftar_contak.append(tambah)
+    elif menu == "3":
+        Function.hapus(daftar_contak)
+
+print(" ")
+print("Program selesai berjalan, Sampai Jumpa")
+print(" ")
+
+#untuk menampilkan daftar kontak, kita akan membuat function / method di file function.py
--- Bahasa Pemrograman/Python/Dasar/Programmer Zaman Now/Function.py
+++ Bahasa Pemrograman/Python/Dasar/Programmer Zaman Now/Function.py
@@ -1,6 +1,46 @@
-def tambah(nama, email, telepon):
-    
+def display(daftar_contak):
+    for kontak in daftar_contak:
+        print(" ")
+        print("Daftar Kontar yang ada : ")
+        print("==========================")
+        print(f"Nama         : {kontak['nama']}")
+        print(f"Email        : {kontak['email']}")
+        print(f"No Telp      : {kontak['telepon']}")
+        print(" ")
 
-def hapus():
+def tambah():
+    nama    = input("Masukkan Nama Kontak : ")
+    email   = input("Masukkan Email Anda : ")
+    telepon = input("Masukkan Telepon Anda : ")
+    kontak  = {
+        "nama"      : nama,
+        "email"     : email,
+        "telepon"   : telepon
+    } 
+    return kontak
 
-def cari():
+def hapus(daftar_contak):
+    nama = input("Masukkan Nama Yang Akan Di Hapus : ")
+    # kita cari nama yang akan di hapus itu berada di index keberapa. caranya seperti berikut :
+
+    # ketika kita mencari nama di dalam list, bisa saja tidak ketemu. oleh karena itu kita harus 
+    # membuat kondisi data yang dicari tidak ketemu terlebih dahulu. caranya kita buat variabel dengan 
+    # nilai negatif 1. di inde itu tidak ada nilai -1.  
+
+    index = -1
+
+    # melakukan pencarian nama dengan perulangan for. ketika tidak menggunakan for terhadap list, 
+    # melainkan for terhadap range
+
+    for i in range(0, len(daftar_contak)):
+        nama = daftar_contak[i]
+        if nama == nama["nama"]:
+            index = 1
+            break
+
+    if index == -1:
+        print("Data Nama Tidak Di Temukan !")
+    else:
+        print("Berhasil hapus Kontak !")
+
+
